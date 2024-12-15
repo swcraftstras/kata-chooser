25dd234f2dc6d2fef0b0eb962f6ade9e1bd85db3
Marc Bouvier <m.bouvier.dev@gmail.com>
2024-12-15 18:33:23 +0100

    Test starter for real domain

---
+++ modules/kata.test.js
@@ -0,0 +1,10 @@
+import {assertEquals} from "@std/assert";
+import { describe, it } from "jsr:@std/testing/bdd";
+import {} from "./kata.js";
+
+describe("foo", () => {
+    it("bar", () => {
+        assertEquals(1, 1);
+    });
+})
+
