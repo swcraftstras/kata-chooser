3c5cf95cd197e286bf0da28b88a8536f98593662
Marc Bouvier <m.bouvier.dev@gmail.com>
2024-12-15 18:37:30 +0100

    Example of documentation test

--- modules/add.js
+++ modules/add.js
@@ -1,3 +1,19 @@
+/**
+ * Example of documentation tests
+ * https://docs.deno.com/runtime/fundamentals/testing/#documentation-tests
+ *
+ * # Examples
+ *
+ * ```ts
+ * import { assertEquals } from "jsr:@std/assert/equals";
+ *
+ * const sum = add(1, 2);
+ * assertEquals(sum, 3);
+ * ```
+ * @param a
+ * @param b
+ * @return {*}
+ */
 export function add(a,b){
     return a + b;
 }
